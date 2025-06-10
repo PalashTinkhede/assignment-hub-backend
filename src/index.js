a1d1bd721a4aab6f66732cd49d3ffa1ea5db1106
PALASH TINKHEDE <150069058+PalashTinkhede@users.noreply.github.com>
2025-06-10 16:28:11 +0530

    add frontend url

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser()); 
 
 app.use(cors({
-  origin:true,
+  origin:'https://assignment-hub-frotend.vercel.app/',
   methods:[ "GET","POST","PUT","DELETE"],
   credentials:true
 }))
